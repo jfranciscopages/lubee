867f23167a89faf47108db68269ec8725176a061
Francisco <zextfpages@correoargentino.com.ar>
2025-12-01 21:36:26 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lubee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListingController : ControllerBase
+    {
+    }
+}
---
+++ Models/Listing.cs
@@ -0,0 +1,15 @@
+﻿namespace lubee.Models
+{
+    public class Publication
+    {
+        public int Id { get; set; }
+        public string PropertyType { get; set; }
+        public string OperationType { get; set; }
+        public string Description { get; set; }
+        public int Rooms { get; set; }
+        public double SquareMeters { get; set; }
+        public int Age { get; set; }
+        public string Coordinates { get; set; }
+        public List<string> Images { get; set; } = new();
+    }
+}
